7721bd4ec475262ade320fc61c114c2a81874bad
Yael Amar <amar.yael2222@gmail.com>
2020-05-25 19:19:48 +0300

    שיעור 12

---
+++ Lesson12/Lesson12/Dog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    class Dog
+    {
+        public int ID { get; set; }
+        public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ID == (obj as Dog).ID)
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
+
+        public static bool operator >(Dog d1, Dog d2)
+        {
+            if (d1.Age > d2.Age)
+                return true;
+            return false;
+        }
+
+        public static bool operator <(Dog d1, Dog d2)
+        {
+            if (d1.Age > d2.Age)
+                return false;
+            return true;
+        }
+
+        public static Dog operator ++(Dog dog)
+        {
+            dog.Age++;
+            return dog;
+        }
+
+
+    }
+}
---
+++ Lesson12/Lesson12/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog() { ID = 1, Age = 15 };
+            Dog dog2 = new Dog() { ID=2,Age=10};
+
+
+            Console.WriteLine(dog1>dog2);
+            Console.WriteLine(dog1++);
+            Console.WriteLine(dog1.Age);
+
+            Stack stack = new Stack();
+            stack.Push(3);
+            stack.Push(dog1);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Queue queue = new Queue();
+
+
+            List<int> l1 = new List<int>();
+            l1.Add(5);
+
+
+            List<Dog> l2 = new List<Dog>();
+            l2.Add(dog1);
+            l2.Add(dog2);
+            foreach (Dog item in l2)
+            {
+                Console.WriteLine(item.Age);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
