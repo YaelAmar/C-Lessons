e3e1772d6924deee4e9264727894bbec02ed6cb4
Yael Amar <amar.yael2222@gmail.com>
2020-06-10 11:25:39 +0300

    שיעור 16

---
+++ Lesson16/Lesson16/PointList.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16
+{
+    class PointList : ArrayList
+    {
+        public override object this[int index] { get => base[index]; set => base[index] = value; }
+
+        public override int Capacity { get => base.Capacity; set => base.Capacity = value; }
+
+       
+        public override int Count => base.Count;
+
+
+        public override bool IsFixedSize => base.IsFixedSize;
+
+        public override bool IsReadOnly => base.IsReadOnly;
+
+        public override bool IsSynchronized => base.IsSynchronized;
+
+        public override object SyncRoot => base.SyncRoot;
+
+        public override int Add(object value)
+        {
+            return base.Add(value);
+        }
+
+        public override void AddRange(ICollection c)
+        {
+            base.AddRange(c);
+        }
+
+        public override int BinarySearch(int index, int count, object value, IComparer comparer)
+        {
+            return base.BinarySearch(index, count, value, comparer);
+        }
+
+        public override int BinarySearch(object value)
+        {
+            return base.BinarySearch(value);
+        }
+
+        public override int BinarySearch(object value, IComparer comparer)
+        {
+            return base.BinarySearch(value, comparer);
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+        }
+
+        public override object Clone()
+        {
+            return base.Clone();
+        }
+       
+
+        public override bool Contains(object item)
+        {
+            return base.Contains(item);
+        }
+
+        public override void CopyTo(Array array)
+        {
+            base.CopyTo(array);
+        }
+
+        public override void CopyTo(Array array, int arrayIndex)
+        {
+            base.CopyTo(array, arrayIndex);
+        }
+
+        public override void CopyTo(int index, Array array, int arrayIndex, int count)
+        {
+            base.CopyTo(index, array, arrayIndex, count);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override IEnumerator GetEnumerator()
+        {
+            return base.GetEnumerator();
+        }
+
+        public override IEnumerator GetEnumerator(int index, int count)
+        {
+            return base.GetEnumerator(index, count);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override ArrayList GetRange(int index, int count)
+        {
+            return base.GetRange(index, count);
+        }
+
+        public override int IndexOf(object value)
+        {
+            return base.IndexOf(value);
+        }
+
+        public override int IndexOf(object value, int startIndex)
+        {
+            return base.IndexOf(value, startIndex);
+        }
+
+        public override int IndexOf(object value, int startIndex, int count)
+        {
+            return base.IndexOf(value, startIndex, count);
+        }
+
+        public override void Insert(int index, object value)
+        {
+            base.Insert(index, value);
+        }
+
+        public override void InsertRange(int index, ICollection c)
+        {
+            base.InsertRange(index, c);
+        }
+
+        public override int LastIndexOf(object value)
+        {
+            return base.LastIndexOf(value);
+        }
+
+        public override int LastIndexOf(object value, int startIndex)
+        {
+            return base.LastIndexOf(value, startIndex);
+        }
+
+        public override int LastIndexOf(object value, int startIndex, int count)
+        {
+            return base.LastIndexOf(value, startIndex, count);
+        }
+
+        public override void Remove(object obj)
+        {
+            base.Remove(obj);
+        }
+
+        public override void RemoveAt(int index)
+        {
+            base.RemoveAt(index);
+        }
+
+        public override void RemoveRange(int index, int count)
+        {
+            base.RemoveRange(index, count);
+        }
+
+        public override void Reverse()
+        {
+            base.Reverse();
+        }
+
+        public override void Reverse(int index, int count)
+        {
+            base.Reverse(index, count);
+        }
+
+        public override void SetRange(int index, ICollection c)
+        {
+            base.SetRange(index, c);
+        }
+
+        public override void Sort()
+        {
+            base.Sort();
+        }
+
+        public override void Sort(IComparer comparer)
+        {
+            base.Sort(comparer);
+        }
+
+        public override void Sort(int index, int count, IComparer comparer)
+        {
+            base.Sort(index, count, comparer);
+        }
+
+        public override object[] ToArray()
+        {
+            return base.ToArray();
+        }
+
+        public override Array ToArray(Type type)
+        {
+            return base.ToArray(type);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override void TrimToSize()
+        {
+            base.TrimToSize();
+        }
+    }
+}
---
+++ Lesson16/Lesson16/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region x
+            //List<Point> points = new List<Point>();
+            //Random random = new Random();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    points.Add(new Point() { X = random.Next(1000), Y = random.Next(500) });
+            //    Console.WriteLine(points[i].X+ " "+points[i].Y);
+            //}
+            //Console.WriteLine("****************************");
+            //var a = points.Select(p => p.X);
+            //foreach (var item in a)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
+
+            // LINQ - Language Integrated Query - שאילתא משולבת בשפה
+
+
+            //טיפוס אנונימי
+            var x=5;
+            var y = new Point();
+
+            List<Point> L = new List<Point>();
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                L.Add(new Point() { X = rnd.Next(1000), Y = rnd.Next(1000) });
+            }
+
+            var L2 = from p in L
+                     where p.X > 100 && p.Y > 100
+                     select p;
+
+            //lambda expression
+            var l3 = L.Where(p=>p.X < 300 && p.Y < 300).ToList();
+            var l4 = L.Where(p =>
+            {
+                if (p.X > 150)
+                    Console.WriteLine("larger than 150");
+                if (p.X < 100)
+                    return true;
+                return false;
+            }
+            ).ToList();
+
+
+           
+
+            foreach (var item in l3)
+            {
+                Console.WriteLine(item.X + " , " + item.Y);
+            }
+
+            Console.WriteLine("******************");
+            Print(L);
+
+            Console.WriteLine("large than 100");
+
+            foreach (var item in L2)
+            {
+                Console.WriteLine(item.X + " , " + item.Y);
+            }
+            Console.ReadLine();
+        }
+
+        public static void Print(List<Point> l)
+        {
+            foreach (var item in l)
+            {
+                Console.WriteLine(item.X + " , " + item.Y);
+            }
+        }
+
+        public static bool GetPoint(Point p)
+        {
+            if (p.X < 300 && p.Y < 300)
+                return true;
+            return false;
+        }
+
+     
+    }
+}
+//http://www.corner.co.il/Articles/ArticleDetials/49/Linq-To-Object--%D7%97%D7%9C%D7%A7-%D7%A8%D7%90%D7%A9%D7%95%D7%9F
+//https://webmaster.org.il/articles/aspnet-linq-syntax/
