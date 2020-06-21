88010480ffedf02a0301236d0dad7ae41b0584fe
Yael Amar <amar.yael2222@gmail.com>
2020-06-21 23:02:03 +0300

    שיעור 19 ואחרון!!!

---
+++ Lesson19/Lesson19/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Lesson19
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(435, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 396);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Lesson19/Lesson19/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson19
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.RightToLeft = RightToLeft.Yes;
+            button.Location = new Point(100, 100);
+            Controls.Add(button);
+            button.Text = "אישור";
+        }
+        //  GroupBox groupBox = new GroupBox();
+        Button button = new Button();
+        Button[] KeyBoard { get; set; } = new Button[27];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // groupBox.Size = new Size(500, 250);
+            //  Controls.Add(groupBox);
+            char s = 'א';
+
+            for (int i = 0; i < KeyBoard.Length; i++)
+            {
+
+                KeyBoard[i] = new Button();
+                KeyBoard[i].Text = s.ToString();
+                s++;
+                if (i < 10)
+                    KeyBoard[i].Location = new Point(i * 30, 10);
+                if (i >= 10 && i < 20)
+                    KeyBoard[i].Location = new Point((i - 10) * 30, 40);
+                if (i >= 20)
+                    KeyBoard[i].Location = new Point((i - 20) * 30, 70);
+                KeyBoard[i].Size = new Size(30, 20);
+
+                KeyBoard[i].Click += new System.EventHandler(KeyBoard_Click);
+
+                Controls.Add(KeyBoard[i]);
+            }
+
+        }
+
+        private void KeyBoard_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += (sender as Button).Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (IsDigit(textBox1.Text))
+                errorProvider1.SetError(textBox1, "אין להכיל מספרים");
+            else
+                errorProvider1.Clear();
+        }
+
+        private bool IsDigit(string s)
+        {
+            if (s.IndexOf("0") != -1 || s.IndexOf("1") != -1)
+                return true;
+            return false;
+        }
+    }
+}
