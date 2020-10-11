130db62b8a43e8f219b7419c99cddb646aebb3d7
Yael Amar <amar.yael2222@gmail.com>
2020-10-11 19:55:45 +0300

    שיעור 2

---
+++ Lesson2b/Lesson2b/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.OutputEncoding = new UTF8Encoding();
+            Console.WriteLine("\u05D0\u05D1");
+            Console.WriteLine("אריאל");
+            Console.WriteLine(new string("אריאל".Reverse().ToArray()));
+
+
+            Console.WriteLine("עברית");
+            Console.ReadLine();
+
+           
+        }
+    }
+}
