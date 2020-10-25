f6a6b776a94fcfc87d9611921a2bb0ec85423639
Yael Amar <amar.yael2222@gmail.com>
2020-10-25 19:38:13 +0200

    עזרה בשיעורי בית

---
+++ hw/hw/Milk.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw
+{
+    class Milk
+    {
+       
+
+        public static int Counter { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public int MyProperty3 { get; set; }
+
+        public Milk(int myProperty, int myProperty2, int myProperty3)
+        {
+            MyProperty = myProperty;
+            MyProperty2 = myProperty2;
+            MyProperty3 = myProperty3;
+        }
+        public Milk()
+        {
+
+        }
+        public Milk(Milk s)
+        {
+            MyProperty = s.MyProperty;
+            MyProperty2 =s. MyProperty2;
+            MyProperty3 =s.MyProperty3;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(Counter+MyProperty);
+        }
+    }
+}
---
+++ hw/hw/ShoppingList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw
+{
+    class ShoppingList
+    {
+        public const string BSD = "בס\"ד";
+        public string Name { get; set; }
+        public DateTime DateOfList { get; set; }
+        public Bread[] Breads { get; set; }
+        public Milk[] Milks { get; set; }
+
+        public ShoppingList(int b,int m)
+        {
+            Milks = new Milk[m];
+            Breads = new Bread[b];
+        }
+
+
+    }
+}
